20b761fd74574ab27ec93b3b84fb98fca3a89292
chuahziyang <czyang2002@gmail.com>
2021-10-22 08:30:05 +0800

    removed SIGTERM

--- src/index.ts
+++ src/index.ts
@@ -58,6 +58,4 @@ if (process.env.NODE_ENV === "production") {
 
 safety();
 
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
